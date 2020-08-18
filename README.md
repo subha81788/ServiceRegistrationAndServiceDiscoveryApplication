497af700f470445210344c113462aa908f52f17d
subha81788 <subha81788@gmail.com>
2020-08-18 08:16:42 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This project demonstrates Service Registry and Service Discovery using spring cloud project.
---
+++ src/main/java/org/subhashis/mycroservices/serviceregistrationanddiscoveryservice/ServiceRegistrationAndDiscoveryServiceApplication.java
@@ -0,0 +1,15 @@
+package org.subhashis.mycroservices.serviceregistrationanddiscoveryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class ServiceRegistrationAndDiscoveryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceRegistrationAndDiscoveryServiceApplication.class, args);
+	}
+	
+}
